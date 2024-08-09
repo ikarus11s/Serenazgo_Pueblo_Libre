52d6f640822b9093f25db82eba9d4c6fe5dd4489
ikarus11s <ismaelsacha9@gmail.com>
2024-08-09 17:36:16 -0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 17:32:18 2024
+
+@author: MNahui
+"""
+
+
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1gS6ZS6lS7Mc5B4TFI8HEK80xq4LANS6nU2O8V8-hEC8/edit?usp=sharing"
+
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+
+data = conn.read(spreadsheet=url, usecols=[:])
+
+
+
+st.dataframe(data)
