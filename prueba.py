597ac73757ac24372948699d9362494a1d7da74c
ikarus11s <ismaelsacha9@gmail.com>
2024-08-09 18:11:53 -0500

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -6,22 +6,15 @@ Created on Fri Aug  9 17:32:18 2024
 """
 
 
+
+# example/st_app.py
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
-import pandas as pd
 
-# URL de la hoja de cálculo
 url = "https://docs.google.com/spreadsheets/d/1gS6ZS6lS7Mc5B4TFI8HEK80xq4LANS6nU2O8V8-hEC8/edit?usp=sharing"
 
-# Conexión con Google Sheets
 conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-# Leer el rango específico: primeras 10 columnas (A-J) hasta la fila 50
-range_ = 'A1:J50'
-data = conn.read(spreadsheet=url, range_=range_)
-
-# Convertir los datos a un DataFrame de pandas
-df = pd.DataFrame(data)
-
-# Mostrar los datos
-st.dataframe(df)
+data = conn.read(spreadsheet=url, usecols=[0, 1])
+st.dataframe(data)
