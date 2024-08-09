ca4748013fd8f9ccc2ce0c93db6c6e67edce64a1
ikarus11s <ismaelsacha9@gmail.com>
2024-08-09 17:39:22 -0500

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -1,42 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 17:32:18 2024
+
+@author: MNahui
+"""
+
+
 import streamlit as st
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
-def main():
-    # Configuración de la página
-    st.title("Acceso a Google Sheets con Streamlit")
-    st.write("Esta aplicación permite leer y escribir datos en una hoja de cálculo de Google Sheets.")
+url = "https://docs.google.com/spreadsheets/d/1gS6ZS6lS7Mc5B4TFI8HEK80xq4LANS6nU2O8V8-hEC8/edit?usp=sharing"
 
-    # Leer credenciales desde los secretos de Streamlit
-    json_creds = st.secrets["gspread"]["json"]
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
 
-    # Configuración de credenciales y acceso
-    scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(json_creds, scope)
-    client = gspread.authorize(creds)
+data = conn.read(spreadsheet=url, usecols=[:])
 
-    try:
-        # Acceso a la hoja de cálculo
-        spreadsheet = client.open('Prueba')  # Nombre del documento de Google Sheets
-        sheet = spreadsheet.worksheet('Hoja 1')  # Nombre de la hoja dentro del documento
-        
-        # Leer datos y convertir a DataFrame
-        data = pd.DataFrame(sheet.get_all_records())
-        st.write("Datos leídos desde Google Sheets:")
-        st.dataframe(data)  # Usar st.dataframe() para una vista interactiva
-        
-        # Escribir datos
-        if st.button('Agregar datos'):
-            sheet.append_row(['Nuevo', 'Dato', 'Ejemplo'])
-            st.write("Datos agregados exitosamente.")
-        
-    except gspread.SpreadsheetNotFound:
-        st.error("No se pudo encontrar la hoja de cálculo. Verifica el nombre y el acceso.")
-    except gspread.WorksheetNotFound:
-        st.error("No se pudo encontrar la hoja dentro de la hoja de cálculo. Verifica el nombre de la hoja.")
-    except Exception as e:
-        st.error(f"Ocurrió un error: {e}")
 
-if __name__ == "__main__":
-    main()
+
+st.dataframe(data)
